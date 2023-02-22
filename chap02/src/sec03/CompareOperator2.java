e40bc055582d84f10aa0f7a2f201b971ad9645a4
505 <505@DESKTOP-32OOQDU>
2023-02-22 16:51:37 +0900

    First Commit

---
+++ chap02/src/sec03/CompareOperator2.java
@@ -0,0 +1,30 @@
+package sec03;
+
+import java.util.Scanner;
+
+public class CompareOperator2 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		char ch = ' ';  //char Type은 반드시 ' '로 초기화(''아님)
+		
+		System.out.printf("영문자와 숫자 또는 한글을 입력하세요.");
+		String input = scanner.nextLine(); //한 문장을 통째로 읽어온다. (문자열)
+		ch = input.charAt(0); //읽어온 문자열에서 맨 첫 글자를 취한다.
+		
+		//숫자
+		if('0'<= ch && ch <= '9') {
+			System.out.printf("key- %c 매핑 유니코드는 %d 입니다.", ch, (int)ch);
+		}
+		//영소문자
+		if('a'<= ch && ch <= 'z') {
+			System.out.printf("key- %c 매핑 유니코드는 %d 입니다.", ch, (int)ch);
+		}
+		//영대문자
+		if('A'<= ch && ch <= 'Z') {
+			System.out.printf("key- %c 매핑 유니코드는 %d 입니다.", ch, (int)ch);
+		}
+		
+	}
+
+}
---
+++ chap02/src/sec03/StringConcat.java
@@ -0,0 +1,25 @@
+package sec03;
+
+public class StringConcat {
+	
+	public static void main(String[] args) {
+		//숫자 연산
+		int value = 10 + 2 + 8;
+		System.out.println("value: " + value);
+		
+		//결합 연산(왼쪽 부터 순서대로)
+		String str1 = 10 + 2 + "8";
+		System.out.println("str1" + str1);  // 128
+		
+		String str2 = 10 + "2" + 8;
+		System.out.println("str2: " + str2);  //1028
+		
+		String str3 = "10" + 2 + 8;
+		System.out.println("str3: " + str3);  // 1028
+		
+		String str4 = "10" + (2 + 8);  // 괄호 먼저 연산
+		System.out.println("str4: " + str4);  //1010
+		
+	}
+
+}
